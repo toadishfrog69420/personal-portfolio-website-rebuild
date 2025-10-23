566073a578bfacf2086caa3a22a1d3e7539fd4c5
Sean Yang <sean.yang@thras.io>
2025-10-23 18:00:14 +0800

    Remove the section divider for now

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <section className="antialiased max-w-6xl mx-2 mt-8 lg:mx-auto flex-auto min-w-0 mt-6 flex flex-col lg:px-4 px-2">
         
-            <section className='divide-y divide-neutral-400 my-4 snap-start'>
+            <section>
                 <div className={`my-8 flex justify-center flex-col`}>
                     <Education />
                 </div>
